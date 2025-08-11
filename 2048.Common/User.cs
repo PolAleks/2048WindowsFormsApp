f32197a131e6db5b6587d8497ffa6a3e42bd2acb
PolAleks <aa.polushkin@yandex.ru>
2025-08-11 13:41:29 +0300

    Добавил интерфейс IConverter

--- 2048.Common/User.cs
+++ 2048.Common/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2048.Common
+﻿namespace _2048.Common
 {
     public class User
     {
