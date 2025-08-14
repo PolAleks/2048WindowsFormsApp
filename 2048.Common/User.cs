238c465654d6cb22650395e78f06d595365a29a5
PolAleks <aa.polushkin@yandex.ru>
2025-08-14 12:56:50 +0300

    Реализовал форму представления для ввода имени игрока

--- 2048.Common/User.cs
+++ 2048.Common/User.cs
@@ -2,6 +2,15 @@
 {
     public class User
     {
+        public User()
+        {
+
+        }
+        public User(string name) : this(name, 0)
+        {
+            Name = name;
+        }
+
         public User(string name, int score)
         {
             Name = name;
---
+++ 2048WindowsFormsApp/FormWelcom.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace _2048WindowsFormsApp
+{
+    partial class FormWelcom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWelcom = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelWelcom
+            // 
+            this.labelWelcom.AutoSize = true;
+            this.labelWelcom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWelcom.Location = new System.Drawing.Point(42, 56);
+            this.labelWelcom.Name = "labelWelcom";
+            this.labelWelcom.Size = new System.Drawing.Size(288, 16);
+            this.labelWelcom.TabIndex = 0;
+            this.labelWelcom.Text = "Для запуска игры напишите свое имя:";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUserName.Location = new System.Drawing.Point(45, 107);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(285, 22);
+            this.textBoxUserName.TabIndex = 1;
+            this.textBoxUserName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUserName_KeyDown);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(103, 167);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(148, 33);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "&Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // FormWelcom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 253);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.labelWelcom);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormWelcom";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Представтесь";
+            this.Load += new System.EventHandler(this.FormWelcom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWelcom;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
